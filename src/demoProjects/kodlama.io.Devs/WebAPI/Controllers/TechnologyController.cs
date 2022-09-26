2e84ada3abe42046691009acc4ae9b554fa3d6cb
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-09-26 15:52:41 +0300

    feat: Added to list technologies

---
+++ src/demoProjects/kodlama.io.Devs/WebAPI/Controllers/TechnologyController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Features.Technologies.Models;
+using Application.Features.Technologies.Queries.GetListTechnology;
+using Application.Features.Technologies.Queries.GetListTechnologyByDynamic;
+using Core.Application.Requests;
+using Core.Persistence.Dynamic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TechnologyController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListTechnologyQuery getListTechnologyQuery = new GetListTechnologyQuery { PageRequest = pageRequest};
+            TechnologyListModel result = await Mediator.Send(getListTechnologyQuery);
+            return Ok(result);
+        }
+
+        [HttpPost("GetList/ByDynamic")]
+        public async Task<ActionResult> GetListByDynamic([FromQuery] PageRequest pageRequest, [FromBody] Dynamic dynamic)
+        {
+            GetListTechnologyByDynamicQuery getListTechnologyByDynamicQuery = new GetListTechnologyByDynamicQuery { PageRequest = pageRequest, Dynamic = dynamic};
+            TechnologyListModel result = await Mediator.Send(getListTechnologyByDynamicQuery);
+            return Ok(result);
+        }
+    }
+}
